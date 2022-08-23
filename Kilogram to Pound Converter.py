49936a080ea4a74223ed10f9b804b1e7fdac495a
eighthhokage <111888225+eighthhokage@users.noreply.github.com>
2022-08-23 10:54:26 -0500

    Add files via upload

---
+++ Kilogram to Pound Converter.py
@@ -0,0 +1,7 @@
+weight = float(input(("How many kilograms do you weigh?")))
+weight = weight * 2.20462
+print("You weigh", weight , "pounds")
+if weight >= 200:
+    print("Maybe you should lose some weight! :p")
+
+
